3da284f3c54b23593541942569e757e8ce115a9c
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-08-19 22:33:31 +0300

    Completed on Aug 19,struggled to complete last function

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(someValue = `roller-skate`){
+    return `This Saturday, I want to ${someValue}!`;
+}
+
+function mondayWork(someValue = `go to the office`){
+    return `This Monday, I will ${someValue}.`;
+}
+
+function wrapAdjective(someValue){
+    if(wrapAdjective == '*'){
+        const innerFunction = function(result){
+            return `You are ${wrapAdjective}${result}${wrapAdjective}!`;
+        }
+    }else if(wrapAdjective == '||' ){
+        const innerFunction = function(result){
+            return `You are ${wrapAdjective}${result}${wrapAdjective}!`;
+        }
+    }
+    
+}
+
+
